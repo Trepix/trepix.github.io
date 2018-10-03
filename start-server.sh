f83a95068414d8232edd7513c4a205ba93ad8eda
Unknown <pau.trepat.segura@gmail.com>
2018-10-04 00:54:23 +0200

    Change start-server.sh to use a docker container

--- start-server.sh
+++ start-server.sh
@@ -1,1 +1,7 @@
-bundle exec jekyll serve
+export JEKYLL_VERSION=3.8 && \
+docker run --rm -it \
+--volume="$PWD:/srv/jekyll" \
+-p 4000:4000 \
+-e JEKYLL_ENV='production' \
+jekyll/jekyll:$JEKYLL_VERSION \
+jekyll serve --watch
