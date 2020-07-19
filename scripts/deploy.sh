94075ea62011ed1789752f7c5743565f732b1bac
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2020-07-19 19:23:13 +0200

    Fix script messages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
+echo "Checking out master branch into public"
 git worktree add -B master public origin/master
 
 echo "Removing existing files"
@@ -21,7 +21,7 @@ rm -rf public/*
 echo "Generating site"
 make build
 
-echo "Updating gh-pages branch"
+echo "Updating MASTER branch"
 hash=`git log --pretty=format:'%h' -n 1`
 cd public && git add --all && git commit -m "[${hash}] Automatic publish"
 
